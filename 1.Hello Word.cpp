ad0cb250d4d37e9c5c1da54872d3a15ccd0d9c9b
waqarasghar-007 <76770549+waqarasghar-007@users.noreply.github.com>
2022-06-13 11:34:26 +0500

    C++ program to print hello word using printf or cout statement in c++

---
+++ 1.Hello Word.cpp
@@ -0,0 +1,14 @@
+//Input output stream to take in and take out data from RAM. This file is located in your compiler setup directory
+#include <iostream>
+//using standard library for cin and cout
+using namespace std;
+//the main function form where program execution/compilation begins
+int main()
+{
+	cout<<"Hello world! \n" ;
+	printf("hello world! \n");
+	
+	system("pause");
+	return 0;
+	
+}
